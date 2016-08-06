7acf470de669c3cafc7eba342f5ab5fad6b52be8
mcclainandrew <transbibilfobil@gmail.com>
2016-08-06 13:05:52 -0700

    Added house based agendas and school wide agenda

--- lua/darkrp_customthings/agendas.lua
+++ lua/darkrp_customthings/agendas.lua
@@ -9,3 +9,10 @@ AddAgenda(Title of the agenda, Manager (who edits it), {Listeners (the ones who 
 ---------------------------------------------------------------------------]]
 -- Example: AddAgenda("Gangster's agenda", TEAM_MOB, {TEAM_GANG})
 -- Example: AddAgenda("Police agenda", TEAM_MAYOR, {TEAM_CHIEF, TEAM_POLICE})
+
+
+AddAgenda("Gryffindor Agenda", TEAM_GRYFFINDOR, {TEAM_GRYFFINDOR})
+AddAgenda("Slytherin Agenda", TEAM_SLYTHERIN, {TEAM_SLYTHERIN})
+AddAgenda("Hufflepuff Agenda", TEAM_HUFFLEPUFF, {TEAM_HUFFLEPUFF})
+AddAgenda("Ravenclaw Agenda", TEAM_RAVENCLAW, {TEAM_RAVENCLAW})
+AddAgenda("Hogwarts Agenda", TEAM_HEADMASTER, {TEAM_GRYFFINDOR, TEAM_SLYTHERIN, TEAM_HUFFLEPUFF, TEAM_RAVENCLAW, TEAMNURSE})

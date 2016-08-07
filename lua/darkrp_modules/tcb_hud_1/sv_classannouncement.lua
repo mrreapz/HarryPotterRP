4f4ae00b284626547bd65bcd45390bb838961799
mcclainandrew <transbibilfobil@gmail.com>
2016-08-07 14:51:16 -0700

    performance fix in class announcements

--- lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
+++ lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
@@ -4,29 +4,26 @@ local classDuration = 30
 
 timer.Create( "ClassTime", classDuration, 0, function()
 	if (true) then
+		houseClass[house.slytherin] = "Test Slytherin class"
+		houseClass[house.gryffindor] = "Test Gryffindor class"
+		houseClass[house.hufflepuff] = "Test Hufflepuff class"
+		houseClass[house.ravenclaw] = "Test Ravenclaw class"
+		local newClass1 = math.random(1,totalClasses)
+		local newClass2 = math.random(1,totalClasses)
+		while (newClass1 == newClass2) do
+			newClass2 = math.random(1,totalClasses)
+		end
+		--currentClassMessage = house.ravenclaw + " : " + class[newClass1] + "\n"
+		--currentClassMessage += house.gryffindor + " : " + class[newClass1] + "\n"
+		--currentClassMessage += house.hufflepuff + " : " + class[newClass2] + "\n"
+		--currentClassMessage += house.slytherin + " : " + class[newClass2] + "\n"
+
+		--houseClass[house.slytherin] = class[newClass1]
+		--houseClass[house.gryffindor] = class[newClass2]
+		--houseClass[house.hufflepuff] = class[newClass1]
+		--houseClass[house.ravenclaw] = class[newClass2]
 		for k,v in pairs(player.GetAll()) do
-			DarkRP.notify(v,0,4,"Class is over, please make your way to your next class")
-			houseClass[house.slytherin] = "Test Slytherin class"
-			houseClass[house.gryffindor] = "Test Gryffindor class"
-			houseClass[house.hufflepuff] = "Test Hufflepuff class"
-			houseClass[house.ravenclaw] = "Test Ravenclaw class"
-			local newClass1 = math.random(1,totalClasses)
-			local newClass2 = math.random(1,totalClasses)
-			while (newClass1 == newClass2) do
-				newClass2 = math.random(1,totalClasses)
-			end
-			
-			--currentClassMessage = house.ravenclaw + " : " + class[newClass1] + "\n"
-			--currentClassMessage += house.gryffindor + " : " + class[newClass1] + "\n"
-			--currentClassMessage += house.hufflepuff + " : " + class[newClass2] + "\n"
-			--currentClassMessage += house.slytherin + " : " + class[newClass2] + "\n"
-
-			--houseClass[house.slytherin] = class[newClass1]
-			--houseClass[house.gryffindor] = class[newClass2]
-			--houseClass[house.hufflepuff] = class[newClass1]
-			--houseClass[house.ravenclaw] = class[newClass2]
-
-
+			DarkRP.notify(v,0,4,"Class is over, please make your way to your next class")			
 		end
 	end
 end)

fa94d96bd89af95113bbc57818928d30758bbd06
mcclainandrew <transbibilfobil@gmail.com>
2016-08-07 13:40:29 -0700

    debug messag ein sv class announcement

--- lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
+++ lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
@@ -6,6 +6,10 @@ timer.Create( "ClassTime", classDuration, 0, function()
 	if (true) then
 		for k,v in pairs(player.GetAll()) do
 			DarkRP.notify(v,0,4,"Class is over, please make your way to your next class")
+			houseClass[house.slytherin] = "Test Slytherin class"
+			houseClass[house.gryffindor] = "Test Gryffindor class"
+			houseClass[house.hufflepuff] = "Test Hufflepuff class"
+			houseClass[house.ravenclaw] = "Test Ravenclaw class"
 			local newClass1 = math.random(1,totalClasses)
 			local newClass2 = math.random(1,totalClasses)
 			while (newClass1 == newClass2) do
@@ -17,10 +21,12 @@ timer.Create( "ClassTime", classDuration, 0, function()
 			--currentClassMessage += house.hufflepuff + " : " + class[newClass2] + "\n"
 			--currentClassMessage += house.slytherin + " : " + class[newClass2] + "\n"
 
-			houseClass[house.slytherin] = class[newClass1]
-			houseClass[house.gryffindor] = class[newClass2]
-			houseClass[house.hufflepuff] = class[newClass1]
-			houseClass[house.ravenclaw] = class[newClass2]
+			--houseClass[house.slytherin] = class[newClass1]
+			--houseClass[house.gryffindor] = class[newClass2]
+			--houseClass[house.hufflepuff] = class[newClass1]
+			--houseClass[house.ravenclaw] = class[newClass2]
+
+
 		end
 	end
 end)

06b2400fbc852994c6ff6728aaaefce458cc59cf
mcclainandrew <transbibilfobil@gmail.com>
2016-08-07 19:43:59 -0700

    implemented the random house pairing for classes

--- lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
+++ lua/darkrp_modules/tcb_hud_1/sv_classannouncement.lua
@@ -9,14 +9,14 @@ house = {
 
 class = {}
 
-class[1] = "Defense Against the Dark Arts"
-class[2] = "Herbology"
-class[3] = "Potions"
-class[4] = "Transfiguration"
-class[5] = "Hagrid's 'Class'"
-class[6] = "Astronomy"
+class[0] = "Defense Against the Dark Arts"
+class[1] = "Herbology"
+class[2] = "Potions"
+class[3] = "Transfiguration"
+class[4] = "Hagrid's 'Class'"
+class[5] = "Astronomy"
 
-totalClasses = 6
+totalClasses = 6 --classes are 0 based, meaning this number will be 1 more than the last class index
 
 houseClass = {}
 houseClass[house.slytherin] = ""
@@ -28,20 +28,36 @@ local classDuration = 30
 
 timer.Create( "ClassTime", classDuration, 0, function()
 	if (true) then
-		local newClass1 = math.random(1,totalClasses)
-		local newClass2 = math.random(1,totalClasses)
+		local newClass1 = math.random(0,totalClasses - 1)
+		local newClass2 = math.random(0,totalClasses - 1)
 		while (newClass1 == newClass2) do
-			newClass2 = math.random(1,totalClasses)
+			newClass2 = math.random(0,totalClasses - 1)
 		end
-		--currentClassMessage = house.ravenclaw + " : " + class[newClass1] + "\n"
-		--currentClassMessage += house.gryffindor + " : " + class[newClass1] + "\n"
-		--currentClassMessage += house.hufflepuff + " : " + class[newClass2] + "\n"
-		--currentClassMessage += house.slytherin + " : " + class[newClass2] + "\n"
 
-		--houseClass[house.slytherin] = class[newClass1]
-		--houseClass[house.gryffindor] = class[newClass2]
-		--houseClass[house.hufflepuff] = class[newClass1]
-		--houseClass[house.ravenclaw] = class[newClass2]
+		local a = {}
+
+	    a[0] = house.slytherin
+		a[1] = house.gryffindor
+		a[2] = house.hufflepuff
+		a[3] = house.ravenclaw
+
+	    for i = 0, 3, +1 
+		do
+            local j = randint( 0, i )
+
+            -- Swap the elements at positions i and j.
+            local temp = a[i]
+            a[i] = a[j]
+            a[j] = temp
+			i++
+        end
+
+		houseClass[a[0]] = newClass1 
+		houseClass[a[1]] = newClass1 
+		houseClass[a[2]] = newClass2 
+		houseClass[a[3]] = newClass2
+
+		
 		for k,v in pairs(player.GetAll()) do
 			local currentJob = v:getDarkRPVar("job")
 

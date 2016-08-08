5654ac1b1eda087f30c0156c33a428f19a6c79ab
mcclainandrew <transbibilfobil@gmail.com>
2016-08-07 23:23:48 -0700

    added code necessary for automatic entity spawn

--- lua/entities/sortinghat/init.lua
+++ lua/entities/sortinghat/init.lua
@@ -15,14 +15,30 @@ function ENT:Initialize()
 	self:SetMoveType( MOVETYPE_VPHYSICS )
 	self:SetSolid( SOLID_VPHYSICS )
 	self:SetUseType( SIMPLE_USE )
+	self:DropToFloor()
 	
 	local phys = self:GetPhysicsObject()
 	if phys:IsValid() then
+		phys:EnableMotion( false )
 		phys:Wake()
 	end
 end
 
-function ENT:Use( activator )
-	activator:SetHealth(50)
-	--activator:changeTeam(TEAM_GRYFFINDOR)
+function ENT:Use( ply, activator )
+	ply:SetHealth(50)
+	ply:changeTeam(TEAM_GRYFFINDOR)
 end
+
+function SortingHatSpawn()
+	local shSpawn = vm.config.mapspawn[ game.GetMap() ]
+	
+	for k, v in pairs( shSpawn ) do
+		local sortinghat = ents.Create( "sortinghat" )
+		sortinghat:SetPos( v.pos )
+		sortinghat:SetAngles( v.ang )
+		sortinghat:SetMoveType( MOVETYPE_NONE )
+		sortinghat:Spawn()
+	end
+end
+
+hook.Add( "InitPostEntity", "SpawnSortingHat", SortingHatSpawn )
